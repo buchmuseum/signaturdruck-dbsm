1598cc0c3cafadbcb1dc88affe0b9cce1c9ef4ed
Ramon Voges <andre.wendler@gmail.com>
2021-12-17 17:33:40 +0100

    drucklabel und vorschau einzeln

--- druck.js
+++ druck.js
@@ -4,5 +4,8 @@ function makelabel() {
     var zeile1 = document.getElementById("zeile1").value,
         zeile2 = document.getElementById("zeile2").value;
 
-    document.querySelector('output').textContent = zeile1 + zeile2;
+    labels = document.querySelectorAll(".label")
+    for (var index = 0; index < labels.length; index++) {
+        labels[index].textContent = zeile1 + zeile2;
+    }
 }
